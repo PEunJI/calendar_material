8e2a8e680644403b903c8c93290f4c4c43b77e03
eunji <68682423a@gmail.com>
2021-08-20 01:26:58 +0900

    카카오 sdk 가져오기 성공

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -11,11 +11,38 @@ import com.example.calendar.ColoredDate.SaturdayDecorator
 import com.example.calendar.ColoredDate.SundayDecorator
 import com.example.calendar.ColoredDate.TodayDecorator
 import com.example.calendar.databinding.ActivityMainBinding
+import android.content.pm.PackageManager
+
+import android.content.pm.PackageInfo
+import android.util.Base64
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //해쉬키 코드로 받기
+        try {
+            val info = packageManager.getPackageInfo(
+                "com.example.calendar",
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val str: String = Base64.encodeToString(md.digest(), Base64.DEFAULT)
+                Log.d("KeyHash:", str)
+                Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        //
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -25,6 +52,7 @@ class MainActivity : AppCompatActivity() {
             TodayDecorator(Context@this),
             AlldayDecorator()
         )
+
 
 
         binding.calendarView.setOnDateChangedListener { widget, date, selected ->
--- app/src/main/java/com/example/calendar/kakaoLogin/KakaoLogin.kt
+++ app/src/main/java/com/example/calendar/kakaoLogin/KakaoLogin.kt
@@ -1,10 +1,11 @@
 package com.example.calendar.kakaoLogin
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 
 class KakaoLogin : Application() {
     override fun onCreate() {
         super.onCreate()
-      //  KakaoSdk.init(this, "20bda91f9ba354f86d1df729923ed50a")
+        KakaoSdk.init(this, "20bda91f9ba354f86d1df729923ed50a")
     }
 }
