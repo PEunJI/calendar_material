30343a9651b280f2fdd0f93cd69963f4bda7e677
eunji <68682423a@gmail.com>
2021-08-19 18:54:58 +0900

    카카오톡 로그인 구현중

---
+++ app/src/main/java/com/example/calendar/kakaoLogin/KakaoLogin.kt
@@ -0,0 +1,10 @@
+package com.example.calendar.kakaoLogin
+
+import android.app.Application
+
+class KakaoLogin : Application() {
+    override fun onCreate() {
+        super.onCreate()
+      //  KakaoSdk.init(this, "20bda91f9ba354f86d1df729923ed50a")
+    }
+}
